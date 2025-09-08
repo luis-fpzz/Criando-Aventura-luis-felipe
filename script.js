a1fb79cc5d57bda252eba42e4177c7d1673743e7
luis-fpz <luis.santos.fernandes@escola.pr.gov.br>
2025-09-08 16:53:03 -0300

    ..

---
+++ script.js
@@ -0,0 +1,8 @@
+const avanca = document. querySelectorAll('.btnproximo');
+
+avanca. forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('ativo');
+        const proximoPasso
+    })
+})
