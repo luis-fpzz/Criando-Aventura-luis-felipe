be8875c3dcf25fa54d8f6dc22fc8aace41a19c7c
luis-fpz <luis.santos.fernandes@escola.pr.gov.br>
2025-09-15 16:02:17 -0300

    ...

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const avanca = document. querySelectorAll('.btnproximo');
 avanca. forEach(button => {
     button.addEventListener('click', function(){
         const atual = document.querySelector('ativo');
-        const proximoPasso
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo')
+
+        atual.Classlist.remove('ativo')
+       document.getElementById('proximoPasso').classList.add('ativo')
+
+
     })
 })
